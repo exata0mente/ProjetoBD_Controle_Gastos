1ae3f1f99eac0beca840983536ad375ea042af4f
Exata0mente <exata0mente@gmail.com>
2019-08-11 18:56:16 -0300

    Adiciona coluna de referência à foreign key

--- SQL/002_CRIA_TABELA_TIPOLANCAMENTOMICRO.sql
+++ SQL/002_CRIA_TABELA_TIPOLANCAMENTOMICRO.sql
@@ -2,6 +2,7 @@
 CREATE TABLE tipoLancamentoMicro(
   codigo        VARCHAR(3) NOT NULL,
   descricao     VARCHAR(55) NOT NULL,
+  codigo_macro  VARCHAR(1) NOT NULL,
   PRIMARY KEY (codigo),
-  FOREIGN KEY (codigo) REFERENCES tipoLancamentoMacro(codigo)
+  FOREIGN KEY (codigo_macro) REFERENCES tipoLancamentoMacro(codigo)
 );
